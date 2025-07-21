f82d495e27d99621ed2dd7cb8e10c5e55ee9e00d
RoureYungLoo <ruoyang8715@163.com>
2025-07-21 11:20:25 +0800

    commit

--- springboot-25-actuator/src/main/java/com/luruoyang/Springboot25ActuatorApplication.java
+++ springboot-25-actuator/src/main/java/com/luruoyang/Springboot25ActuatorApplication.java
@@ -3,6 +3,9 @@ package com.luruoyang;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author luruoyang
+ */
 @SpringBootApplication
 public class Springboot25ActuatorApplication {
 
---
+++ springboot-26-security/src/main/java/com/luruoyang/Springboot26SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.luruoyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot26SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot26SecurityApplication.class, args);
+    }
+
+}
